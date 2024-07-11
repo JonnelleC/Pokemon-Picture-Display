03089efce54b42d7bdf4fd68d664c86d9039734b
JonnelleC <carrjonnelle@gmail.com>
2024-07-11 18:09:57 -0400

    4th commit

--- pokemon.js
+++ pokemon.js
@@ -23,12 +23,11 @@ async function fetchData() {
     const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
 
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
-    
+    const data = await response.json();
 
     if (!response.ok) {
       throw new Error("Could not fet resource");
     }
-    const data = await response.json();
     const pokemonSprite = data.sprites.front_default;
     const imgElement = document.getElementById("pokemonSprite");
     imgElement.src = pokemonSprite;
@@ -36,6 +35,22 @@ async function fetchData() {
   } catch (error) {
     console.error(error);
   }
+
+  const pokemonAbilities =[];
+  for (let i =0; i < fetchData.abilities; i++){
+    pokemonAbilities.push(data.abilities[i].abilities.pokemonName);
+  }
+ 
+  const pokemonLi = document.createElement('li'); 
+
+  pokemonLi.innerHTML = `
+  <h3>${fetchData.name} </h3>
+  <p>${fetchData.name}  </p>
+  <p>${fetchData.name} </p>`
+
+  const orderedList = document.querySelector(`ol`);
+
+  orderedList.append(pokemonLi);
 };
 
 
